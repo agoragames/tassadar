4b4144bf57a5fa0cbb3d3d2e05e7295c003bbafd
David Czarnecki <me@davidczarnecki.com>
2013-01-27 10:15:32 -0800

    Fix version spec

--- spec/tassadar/version_spec.rb
+++ spec/tassadar/version_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe 'Tassadar::VERSION' do
   it 'should be the correct version' do
-    Tassadar::VERSION.should == '0.2.0'
+    Tassadar::VERSION.should == '0.3.0'
   end
 end

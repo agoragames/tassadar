ca256054ea02a3b67606ae93b04640ea9c803e03
Jake Yesbeck <jake@apartmentlist.com>
2015-10-26 22:34:48 -0700

    Update archive_header_spec to rspec 3
    Change instance variable to `let` statement.
    
    Do not use `"should"` in assertion titles, let them speak for
      themselves.
    
    Use `expect` instead of `should`.
    
    Explicitly expect a certain error to be raised.

--- spec/tassadar/mpq/archive_header_spec.rb
+++ spec/tassadar/mpq/archive_header_spec.rb
@@ -1,64 +1,64 @@
 require 'spec_helper'
 
 describe Tassadar::MPQ::ArchiveHeader do
-  before(:each) do
-    @archive_header = Tassadar::MPQ::ArchiveHeader.read("MPQ\x1A,\x00\x00\x00P5\x00\x00\x01\x00\x03\x00\xB03\x00\x00\xB04\x00\x00\x10\x00\x00\x00\n\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
+  let(:archive_header) do
+    Tassadar::MPQ::ArchiveHeader.read("MPQ\x1A,\x00\x00\x00P5\x00\x00\x01\x00\x03\x00\xB03\x00\x00\xB04\x00\x00\x10\x00\x00\x00\n\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
   end
 
   describe "require a valid magic string" do
-    it "should accept a valid magic string" do
+    it "accepts a valid magic string" do
       expect { Tassadar::MPQ::ArchiveHeader.read("MPQ\x1A,\x00\x00\x00P5\x00\x00\x01\x00\x03\x00\xB03\x00\x00\xB04\x00\x00\x10\x00\x00\x00\n\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00") }.to_not raise_error
     end
 
-    it "should not accept an invalid magic string" do
-      expect { Tassadar::MPQ.ArchiveHeader.read("MPQ\x1A,\x00\x00\x00P5\x00\x00\x01\x00\x03\x00\xB03\x00\x00\xB04\x00\x00\x10\x00\x00\x00\n\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00") }.to raise_error
+    it "does not accept an invalid magic string" do
+      expect { Tassadar::MPQ.ArchiveHeader.read("MPQ\x1A,\x00\x00\x00P5\x00\x00\x01\x00\x03\x00\xB03\x00\x00\xB04\x00\x00\x10\x00\x00\x00\n\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00") }.to raise_error(/undefined method/)
     end
   end
 
-  it "should read the header size" do
-    @archive_header.header_size.should == 44
+  it "reads the header size" do
+    expect(archive_header.header_size).to eq(44)
   end
 
-  it "should read the archive size" do
-    @archive_header.archive_size.should == 13648
+  it "reads the archive size" do
+    expect(archive_header.archive_size).to eq(13648)
   end
 
-  it "should read the format version" do
-    @archive_header.format_version.should == 1
+  it "reads the format version" do
+    expect(archive_header.format_version).to eq(1)
   end
 
-  it "should read the sector size shift" do
-    @archive_header.sector_size_shift.should == 3
+  it "reads the sector size shift" do
+    expect(archive_header.sector_size_shift).to eq(3)
   end
 
-  it "should read the hash table offset" do
-    @archive_header.hash_table_offset.should == 13232
+  it "reads the hash table offset" do
+    expect(archive_header.hash_table_offset).to eq(13232)
   end
 
-  it "should read the block table offset" do
-    @archive_header.block_table_offset.should == 13488
+  it "reads the block table offset" do
+    expect(archive_header.block_table_offset).to eq(13488)
   end
 
-  it "should read the hash table entries" do
-    @archive_header.hash_table_entries.value.should > 0
-    @archive_header.hash_table_entries.value.should < 2 ** 20
-    Math.log2(@archive_header.hash_table_entries.to_i).floor.should == Math.log2(@archive_header.hash_table_entries.to_i).ceil
-    @archive_header.hash_table_entries.should == 16
+  it "reads the hash table entries" do
+    expect(archive_header.hash_table_entries.value).to be > 0
+    expect(archive_header.hash_table_entries.value).to be < 2 ** 20
+    expect(Math.log2(archive_header.hash_table_entries.to_i).floor).to eq(Math.log2(archive_header.hash_table_entries.to_i).ceil)
+    expect(archive_header.hash_table_entries).to eq(16)
   end
 
-  it "should read the block table entries" do
-    @archive_header.block_table_entries.should == 10
+  it "reads the block table entries" do
+    expect(archive_header.block_table_entries).to eq(10)
   end
 
-  it "should read the extended_block_table_offset" do
-    @archive_header.extended_block_table_offset.should == 0
+  it "reads the extended_block_table_offset" do
+    expect(archive_header.extended_block_table_offset).to eq(0)
   end
 
-  it "should read the hash table offset high" do
-    @archive_header.hash_table_offset_high.should == 0
+  it "reads the hash table offset high" do
+    expect(archive_header.hash_table_offset_high).to eq(0)
   end
 
-  it "should read the block table offset high" do
-    @archive_header.block_table_offset_high.should == 0
+  it "reads the block table offset high" do
+    expect(archive_header.block_table_offset_high).to eq(0)
   end
 end

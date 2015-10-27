61abccd76235e3454e86cfc3f04876c8ff33a985
Jake Yesbeck <jake@apartmentlist.com>
2015-10-26 22:48:12 -0700

    Update replay_spec
    Assert that fields are not `nil` instead of `true`, as per intention.
    
    Use `let` instead of instance variables
    
    Do not use `"should"` in expectation descriptions, let them speak
      for themselves.
    
    Use `expect` instead of `should`

--- spec/tassadar/sc2/replay_spec.rb
+++ spec/tassadar/sc2/replay_spec.rb
@@ -1,28 +1,25 @@
 require 'spec_helper'
 
 describe Tassadar::SC2::Game do
-  before(:each) do
-    @replay = Tassadar::SC2::Replay.new(File.join(REPLAY_DIR, "patch150.SC2Replay"))
-    @replay_from_data = Tassadar::SC2::Replay.new(nil, File.read(File.join(REPLAY_DIR, "patch150.SC2Replay")))
+  let(:replay) { Tassadar::SC2::Replay.new(File.join(REPLAY_DIR, "patch150.SC2Replay")) }
+  let(:replay_from_data) { Tassadar::SC2::Replay.new(nil, File.read(File.join(REPLAY_DIR, "patch150.SC2Replay"))) }
+
+  it "has the same result" do
+    expect(replay.game.winner.name).to eq(replay_from_data.game.winner.name)
   end
 
-  it "should have the same result" do
-    @replay.game.winner.name.should == @replay_from_data.game.winner.name
+  it "has players" do
+    expect(replay.players).to_not be_nil
+    expect(replay_from_data.players).to_not be_nil
   end
 
-  it "should have players" do
-    @replay.players.should be_true
-    @replay_from_data.players.should be_true
-  end
-  
-  it "should have game" do
-    @replay.game.should be_true
-    @replay_from_data.game.should be_true
-  end
-  
-  it "should have details" do
-    @replay.details.should be_true
-    @replay_from_data.details.should be_true
+  it "has game" do
+    expect(replay.game).to_not be_nil
+    expect(replay_from_data.game).to_not be_nil
   end
 
+  it "has details" do
+    expect(replay.details).to_not be_nil
+    expect(replay_from_data.details).to_not be_nil
+  end
 end
